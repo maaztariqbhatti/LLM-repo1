6ff695fff277a6dd686d38bafd6aaa85a78591c7
Maaz <maaz.bhatti@iceye.fi>
2024-01-15 13:13:11 +0200

    llama path update

--- models/llama_13b_chat_hf/__main__.py
+++ models/llama_13b_chat_hf/__main__.py
@@ -1,4 +1,4 @@
 import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run("api_endpoint:app", host="192.168.9.155", port=8093, reload="true")
+    uvicorn.run("llama13bChat_MLServer:app", host="192.168.9.155", port=8093, reload="true")
